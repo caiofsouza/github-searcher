66c60ca84a1b702639ef156aa22d4f4a66400572
Caio Fernandes <caio_fsouza@hotmail.com>
2017-08-25 02:25:39 -0300

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# GitHub-Searcher
-Terminal searcher/cloner of Github repositories
+# GitHub Searcher
+A simple CLI searcher/cloner of Github repositories
+
+## Install
+```
+$ npm install -g github-searcher
+```
+
+## Usage
+```sh
+github-searcher
+```
+
+Then, input the repository name, or 'owner/repository', and select what you want to clone it to the current path.
--- index.js
+++ index.js
@@ -1,79 +1,149 @@
+#!/usr/bin/env node
+
 const chalk = require('chalk')
-const clear = require('clear')
 const inquirer = require('inquirer')
 const GitHubApi = require('github')
-const git = require('simple-git')()
-const fs = require('fs')
-const files = require('./libs/file')
 const figlet = require('figlet')
-
-const { exec } = require('child_process')
-
-const github = new GitHubApi({
+const ora = require('ora')
+const { spawn } = require('child_process')
+const github =  new GitHubApi({
   version: '3.0.0'
 })
+// unicode of star
+const starCode = '\u2605'
 
-clear()
 console.log(
-  chalk.blue(
-    figlet.textSync('GitHub Finder', { horizontalLayout: 'full' })
-  )
+  chalk.blue(figlet.textSync('GitHub Searcher', { horizontalLayout: 'full' }))
 )
+// some helpers variables
+let currentPage = 1
+let currentRepoName = ''
+let currentRepoArr = []
 
+/**
+ * Show a list to user choose the repository 
+ * that him want to clone
+ * @param {Object} opts List of repositories finded to user choose 
+ */
+function showResultsList (opts) {
+  inquirer.prompt(
+    [
+      {
+        type: 'list',
+        name: 'selected',
+        message: 'Select the repository that you want to clone',
+        choices: opts
+      }
+    ]
+  ).then(answer => {
+    const { selected } = answer
+    if (selected === 'next_page') {
+      // get the next page
+      currentPage += 1
+      listRepoCallback(currentRepoName)
+    } else {
+      // find the selected repo
+      const selectedRepo = currentRepoArr.find(repo => { 
+        return repo.full_name === selected
+      })
+      cloneRepository(selectedRepo)
+    }
+  })
+}
 
-const getGithubRepo = (callback) => {
-  var questions = [
+/**
+ * Clone a repository with a subprocess 
+ * @param {Object} repoObj the repository object
+ */
+function cloneRepository (repoObj) {
+  // init the loader spinner
+  const spinner = ora('Cloning the repository "' + repoObj.full_name + '"').start()
+  const cloneProcess = spawn('git', [ 'clone', repoObj.url])
+  cloneProcess.on('close', (status) => {
+    if (status === 0) {
+      spinner.succeed()
+      process.exit()
+    } else {
+      spinner.fail()
+      switch(status) {
+        case 128:
+          console.log(chalk.red('The repository already exists'))
+          break;
+      }
+      process.exit()
+    }
+  })
+}
+
+/**
+ * Callback after the user chose the repository
+ * @param {String or Object} params The repository name or the repository object
+ */
+function listRepoCallback (params) {
+  currentRepoName = typeof params === 'string' ? params : params.repo_name
+  if (currentRepoName.length <= 2) {
+    console.log(chalk.red('Search a repository name with 2 or more characters'))
+    process.exit()
+  }
+  const spinner = ora('Searching repositories...').start()
+  github.search.repos({
+    q: currentRepoName,
+    order: 'desc',
+    per_page: 20,
+    page: currentPage,
+    sort: 'stars'
+  }, (err, res) => {
+    spinner.stop()
+    if (err) {
+      console.log(err.toString())
+      process.exit()
+    }
+    if (!res.data.items.length) {
+      console.log(chalk.yellow('We couldn’t find any repositories matching "'+currentRepoName+'"'))
+      process.exit()
+    }
+    // map some attributes of repositories
+    currentRepoArr = res.data.items.map(repo => {
+      return {
+        full_name: repo.full_name,
+        url: repo.git_url,
+        description: ( repo.description !== null ? repo.description : '' ),
+        stars: repo.stargazers_count
+      }
+    })
+    // map the repositories
+    const repoFullNames = currentRepoArr.map(repo => {
+      return {
+        name: `${repo.full_name}${ repo.description !== null ? ' - ' + repo.description : '' } (${starCode} ${repo.stars})`,
+        value: repo.full_name,
+        short: `${repo.full_name} (${starCode} ${repo.stars})`
+      }
+    })
+    // if has 20 results on the github search
+    // show a next page option
+    if(repoFullNames.length === 20) {
+      repoFullNames.push({
+        name: '[NEXT PAGE]',
+        value: 'next_page'
+      })
+    }
+    showResultsList(repoFullNames)
+  })
+}
+/**
+ * Show the prompt for input the repository name
+ */
+function promptRepoName() {
+  const questions = [
     {
       name: 'repo_name',
       type: 'input',
-      message: 'Search a GitHub repository'
+      message: 'Type a GitHub repository name or owner/repository'
     }
   ]
-
-  inquirer.prompt(questions).then(callback);
+  inquirer.prompt(questions).then(listRepoCallback)
 }
-
-getGithubRepo((params) => {
-  const { repo_name } = params
-
-  console.log(chalk.blue('Searching repositories...'))
-
-  github.search.repos({
-    q: repo_name
-  }, function(err, res) {
-    const repoFullNames = res.data.items.map(repo => {
-      return repo.full_name
-    })
-
-    inquirer.prompt(
-      [
-        {
-          type: 'list',
-          name: 'selected',
-          message: 'Select the repository that you want to clone?',
-          choices: repoFullNames,
-          default: ['node_modules', 'bower_components']
-        }
-      ]
-    ).then(function( answers ) {
-      const { selected } = answers
-      console.log(selected)
-
-      exec('echo ""', (err, stdout, stderr) => {
-        if (err) {
-          // node couldn't execute the command
-          return;
-        }
-
-        // the *entire* stdout and stderr (buffered)
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
-      })
-
-      process.exit()
-    })
-  })
-  
-})
+// init the client
+promptRepoName()
 
 
